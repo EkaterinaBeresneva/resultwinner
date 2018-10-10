a423f504524e34d8de0a6c4cf50c877daa5cddcf
EkaterinaBeresneva <43822724+EkaterinaBeresneva@users.noreply.github.com>
2018-10-10 09:27:09 +0500

    variant4
    рефакторинг2, вывод в функции

--- Результат победителя/Program.cs
+++ Результат победителя/Program.cs
@@ -10,46 +10,51 @@ namespace Результат_победителя
     {
         static void Main(string[] args)
         {
-       
-            double[,] masrezult = new double[5,3];
-            double[] masbest = new double[5];
+
+            double[,] masrezult = new double[5, 3];
             
-            Random random = new Random();
-            
-            for(int i = 0; i <= 4; i++)
+
+            inputData(masrezult);
+            double max;
+            int p;
+            findMax(masrezult, out max, out p);
+            Console.Write("\n Лучший результат в соревновании по прыжкам с места у Участника {1} [{0:F2}]", max, p + 1);
+            Console.ReadLine();
+        }
+
+        private static void findMax(double[,] masrezult, out double max, out int p)
+        {
+            max = masrezult[0, 0];
+            p = 0;
+            for (int i = 0; i <= 4; i++)
             {
-                Console.WriteLine("\n Участник {0}", i+1);
+                for (int j = 0; j <= 2; j++)
+                {
+                    if (Double.IsNaN(max) || max < masrezult[i, j])
+                    {
+                        
+                        max = masrezult[i, j];
+                        p = i;
+                    }
+                }
+
+            }
+           
+        }
+
+        private static void inputData(double[,] masrezult)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("\n Участник {0}", i + 1);
                 for (int j = 0; j <= 2; j++)
                 {
                     masrezult[i, j] = random.NextDouble() + 1.5;
                     Console.Write("\t[{0:F2}]", masrezult[i, j]);
-                }              
-            }
-            double max = 1;
-            for (int i = 0; i <= 4; i++)
-            {
-                for (int j = 0; j <= 2; j++)
-                {
-                    if(max<masrezult[i,j])
-                    {
-                        masbest[i]= masrezult[i, j];
-                        max = masrezult[i, j];
-                    }
-                }
-                
-            }
-            max = masbest[0];
-            int p = 0;
-            for (int i = 0; i <= 4; i++)
-            {               
-                if (max < masbest[i])
-                {
-                    max = masbest[i];
-                    p = i;
                 }
             }
-            Console.Write("\n Лучший результат в соревновании по прыжкам с места у Участника {1} [{0:F2}]", max, p+1);
-            Console.ReadLine();
         }
     }
 }
